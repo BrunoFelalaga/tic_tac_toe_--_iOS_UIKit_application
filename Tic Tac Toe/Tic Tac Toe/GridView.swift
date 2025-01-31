03c91f41fe58d0e311c6c004351523d31503dec5
BrunoFelalaga <felalagabruno@gmail.com>
2025-01-31 15:20:47 -0600

    adding outlets now

--- Tic Tac Toe/Tic Tac Toe/GridView.swift
+++ Tic Tac Toe/Tic Tac Toe/GridView.swift
@@ -11,5 +11,35 @@ class GridView: UIView {
     
     override func draw(_ rect: CGRect) {
         
+        let verticalLine = UIBezierPath()
+        let leftLineX = rect.width / 3
+        let rightLineX = leftLineX * 2
+        
+        verticalLine.move(to: CGPoint(x: leftLineX, y: 0))
+        verticalLine.addLine(to: CGPoint(x: leftLineX, y: rect.height))
+        
+        verticalLine.move(to: CGPoint(x: rightLineX, y: 0))
+        verticalLine.addLine(to: CGPoint(x: rightLineX, y: rect.height))
+        
+        
+        let horizontalLine = UIBezierPath()
+        let topLineY = rect.height / 3
+        let bottomLineY = topLineY * 2
+        
+        horizontalLine.move(to: CGPoint(x: 0, y: topLineY))
+        horizontalLine.addLine(to: CGPoint(x: rect.width, y: topLineY))
+        
+        horizontalLine.move(to: CGPoint(x: 0, y: bottomLineY))
+        horizontalLine.addLine(to: CGPoint(x: rect.width, y: bottomLineY))
+        
+        
+        UIColor.purple.setStroke()
+        verticalLine.lineWidth = 5.0
+        horizontalLine.lineWidth = 5.0
+        
+        verticalLine.stroke()
+        horizontalLine.stroke()
+        
+        
     }
 }
--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var squares: [UIView]!
+    @IBOutlet var ss: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
