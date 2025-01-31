244e14969c1263b82dc526bf8b367c86a2c4815e
BrunoFelalaga <felalagabruno@gmail.com>
2025-01-31 14:20:12 -0600

    lll

---
+++ Tic Tac Toe/Tic Tac Toe/GridView.swift
@@ -0,0 +1,15 @@
+//
+//  GridView.swift
+//  Tic Tac Toe
+//
+//  Created by Bruno Felalaga on 1/31/25.
+//
+
+import UIKit
+
+class GridView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        
+    }
+}
---
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe
+//
+//  Created by Bruno Felalaga on 1/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
